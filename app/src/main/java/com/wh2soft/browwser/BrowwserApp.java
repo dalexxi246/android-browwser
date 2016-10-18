8a236abe1b7ac2341e8b75e3c49d7e63fa4cb959
Wilmer Hurtado <walex6hh@gmail.com>
2016-10-17 23:20:05 -0500

    init commit

---
+++ app/src/main/java/com/wh2soft/browwser/BrowwserApp.java
@@ -0,0 +1,10 @@
+package com.wh2soft.browwser;
+
+import android.app.Application;
+
+/**
+ * Created by wilmer on 17/10/16.
+ */
+
+public class BrowwserApp extends Application {
+}
---
+++ app/src/main/java/com/wh2soft/browwser/view/ui/BrowserView.java
@@ -0,0 +1,8 @@
+package com.wh2soft.browwser.view.ui;
+
+/**
+ * Created by wilmer on 17/10/16.
+ */
+
+public interface BrowserView {
+}
